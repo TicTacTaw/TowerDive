14e3435c40c4e47ab48c2860a3f50027b6e1cd8b
Thibault Crosnier <thibault.crosnier@ekino.com>
2017-10-04 00:02:28 +0200

    Set freactal as state management lib

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,36 @@
 import React from 'react';
+import { provideState, injectState, softUpdate } from 'freactal';
 import logo from '../logo.svg';
 import './App.css';
 
-const App = () => (
-  <div className="App">
-    <header className="App__header">
-      <img src={logo} className="App__header__logo" alt="logo" />
-      <h1 className="App__header__title">Welcome to React</h1>
-    </header>
-    <p className="App__intro">
-      To get started, edit <code>src/App.js</code> and save to reload.
-    </p>
-  </div>
-);
+const stateInitializer = {
+  initialState: () => ({ localCounter: 0 }),
+  effects: {
+    localAddOne: softUpdate(state => ({
+      localCounter: state.localCounter + 1,
+    })),
+  },
+};
 
-export default App;
+const wrapInState = provideState(stateInitializer);
+
+export default wrapInState(
+  injectState(({ state, effects }) => (
+    <div className="App">
+      <header className="App__header">
+        <img
+          src={logo}
+          className="App__header__logo"
+          alt="logo"
+          onClick={effects.localAddOne}
+        />
+        <h1 className="App__header__title">
+          Welcome to React {state.localCounter}
+        </h1>
+      </header>
+      <p className="App__intro">
+        To get started, edit <code>src/App.js</code> and save to reload.
+      </p>
+    </div>
+  ))
+);
